dfe930a397c9d5578d1cb9ad7e4a734f4de78735
SrabaniS <srabani.suprakash@gmail.com>
2019-04-06 09:41:50 +0100

    my commit

---
+++ src/love/programming/AarryPractice.java
@@ -0,0 +1,53 @@
+package love.programming;
+import java.lang.*;
+import java.util.Arrays;
+
+public class AarryPractice {
+    public static void main(String[] args)
+    {
+        int myintArray;
+        String mystring;
+
+        int [] myintArray1 = new int[100];
+         String [] myString = new String[10];
+       for (int i =0; i< myintArray1.length; i++) {//length is the propoty Aarry
+            i++;
+            myintArray1[i]= i;
+
+           System.out.println( myintArray1[i]);}
+
+
+System.out.println(myintArray1);
+
+System.out.println(Arrays.toString(myintArray1));// to convert  whola arrray into string
+
+          // x int x []= new int [7];//declarlation or inisalition same time
+
+           // x[0]= 10;            // x[1]= 15;
+          ////  x[2]=20;
+           // x[3]=25;
+            //x[4]=25
+            // [5]=35;
+            //x[6]= 40
+         //  String[] str = new String[5];
+           // str[0]= "saazzz";
+            //str[1] = "sayena";
+           // str[2] = "gghhj";
+           // str[3]="gjnkji";
+
+
+            // for (int i=0; i<str.length; i++) {
+               //
+        //  System.out.println(str[i]);
+          //
+        //   }
+            //String[] str ={ "assd", "bjbjhk","cghbkjnfggg","dtfhgjh,","fghgh"};
+          //  for( i=0; i <str.length; i++);
+           // System.out.println(str[i]);
+
+
+
+
+
+    }}
+
